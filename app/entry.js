3acc20c6d5d583747eadb237d3d7aa4e85370060
skaxwng <samkj.ks@gmail.com>
2016-05-15 10:00:56 -0500

    Added text

--- app/entry.js
+++ app/entry.js
@@ -3,12 +3,13 @@ import { FlowRouter } from 'meteor/kadira:flow-router-ssr';
 import { mount } from 'react-mounter';
 
 import Layout from './components/layout';
+import Text from './components/text';
 
 FlowRouter.route('/', {
   name: 'hello',
   action() {
     mount(Layout, {
-      content: () => (<p>View page source</p>),
+      content: () => (<Text />),
     });
   },
 });
