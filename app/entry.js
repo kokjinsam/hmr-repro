7d018af943c55a6e8849d282c0758454bf22b043
skaxwng <samkj.ks@gmail.com>
2016-05-26 23:33:58 -0500

    Upgraded to React 15.1.0, problem persists

--- app/entry.js
+++ app/entry.js
@@ -4,12 +4,12 @@ import { FlowRouter } from 'meteor/kadira:flow-router-ssr';
 import { mount } from 'react-mounter';
 import { AppContainer } from 'react-hot-loader';
 
-import Text from './components/text';
+import ConsoleErrorReporter from './components/console-error-reporter';
 
 const LayoutCtx = (props) => {
   const Layout = require('./components/layout').default;
   return (
-    <AppContainer>
+    <AppContainer errorReport={ConsoleErrorReporter}>
       <LayoutCtx {...props} />
     </AppContainer>
   );
@@ -18,8 +18,18 @@ const LayoutCtx = (props) => {
 FlowRouter.route('/', {
   name: 'hello',
   action() {
+    const Text = require('./components/text').default;
     mount(LayoutCtx, {
       content: () => (<Text />),
     });
   },
 });
+
+if (module.hot) {
+  module.hot.accept([
+    './components/layout',
+    './components/text',
+  ], () => {
+    FlowRouter._current.route._action(FlowRouter._current.params);
+  });
+}
